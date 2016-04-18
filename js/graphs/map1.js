cb59761420f1672c76e270ae4722539f7d2989cc
lmelgar <lxm353@miami.edu>
2016-04-18 08:36:08 -0400

    Uploading project for the first time

---
+++ js/graphs/map1.js
@@ -0,0 +1,192 @@
+/*--------------------------------------------------------------------------
+Scatter: median income & stamp_children_rate
+--------------------------------------------------------------------------*/
+
+(function() {
+
+  var current = "homeless_pop"; // default view
+
+  var width = 320,
+      height = 300;
+
+  // use a d3 map to make a lookup table for the string in the chart title
+  var chartLabels = d3.map();
+    chartLabels.set("homeless_pop", "total homeless student");
+    chartLabels.set("perc_homeless", "percentage of homeless students");
+
+
+  // Not going to lie, there's a ton of trial and error here.
+  var projection = d3.geo.conicEqualArea()
+        .parallels([31,25])
+        .rotate([81, 0])
+        .center([0, 28])
+        .translate([280,160])
+        .scale(2795);
+
+  var path = d3.geo.path()
+      .projection(projection);
+
+  var svg = d3.select("#map1").append("svg")
+      .attr("viewBox", "0 0 " + width + " " + height )
+      .attr("preserveAspectRatio", "xMinYMin slice");
+      /*.attr("width", width)
+      .attr("height", height)*/;
+
+  var myTooltip2 = d3.select("body").append("div").attr("class", "myTooltip2");
+
+  var idLookup = d3.map();
+
+  // all our data is a binary 1 or 2 for this dataset.
+  var colorScale = d3.scale.linear().range(["#EDD8D3", "#B75C57"]);
+
+  // we use queue because we have 2 data files to load.
+  queue()
+    .defer(d3.json, "data/us_counties_topo.json")
+    .defer(d3.csv, "data/dataSet2013.csv", typeAndSet) // process
+    .await(loaded);
+
+  function loaded(error, us, data) {
+    if (error) throw error;
+
+  colorScale.domain(d3.extent(data, function(d) { return +d[current];}));
+
+    var states = topojson.feature(us, us.objects.states).features;
+    var florida = states.filter(function(d) { return d.id == 12;})[0];
+    var counties = topojson.feature(us, us.objects.counties).features;
+    var flcounties = counties.filter(function(d) {
+      return d.id.toString().match(/^12/); // look for counties starting with 12
+    });
+    console.log("counties",flcounties);
+
+    svg.selectAll("path.state")
+      .data(florida)
+      .enter().append("path")
+        .attr("d", path)
+        .attr("class", "state");
+
+    svg.selectAll("path.county")
+        .data(flcounties)
+      .enter().append("path")
+        .attr("d", path)
+        .attr("class", "county")
+        .attr("fill", function(d,i) {
+          var data = idLookup.get(d.id);
+          if (data) {
+            return colorScale(+data[current]);
+          } else {
+            return "darkgray";
+          }
+        })
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseout", mouseout);
+
+        d3.select(window).on('resize', resize);
+
+        function resize() {
+
+
+        }
+
+  d3.selectAll(".btn")
+      .on("click", function() {
+          var item = d3.select(this);
+          d3.selectAll("#buttons .btn").classed("btn-active", false);
+          current = item.attr("id");
+          colorScale.domain(d3.extent(data, function(d) { return +d[current];}));
+
+          legendColors = d3.legend.color()
+              .shapeWidth(20)
+              .labelFormat(d3.format(",.0f"))
+              .scale(colorScale); // our existing color scale
+
+              svg.select(".legendColors")
+              .call(legendColors);
+
+          var label = chartLabels.get(current);
+          item.classed("btn-active", true);
+          item.classed(item.attr("id"), true);
+          updateFill(current);
+          d3.select("h2#chartLabel").text("" + label);
+      });
+  console.log("data", data);
+
+   svg.append("g")
+      .attr("class", "legendColors")
+          .attr("transform", "translate(100, 175)"); // where we put it on the page!
+
+      var legendColors = d3.legend.color()
+      .shapeWidth(20)
+      .labelFormat(d3.format(",.0f"))
+      .scale(colorScale); // our existing color scale
+
+      svg.select(".legendColors")
+      .call(legendColors);
+
+   var range = d3.extent(data, function(d) { return +d[current];});
+
+  } // end loaded;
+
+
+    function updateFill(current) {
+
+      console.log("current", current);
+
+      svg.selectAll("path.county")
+        .transition()
+        .attr("fill", function(d) {
+          var data = idLookup.get(d.id);
+          if (data) {
+            return colorScale(+data[current]);
+          } else {
+            return "darkgray";
+          }
+        });
+    }
+
+    function typeAndSet(d) {
+      // create the lookup hash for the county id in the map (id) and data (fipstxt)
+      idLookup.set(+d.FIPStxt, d);
+      return d;
+    }
+
+    d3.selection.prototype.moveToFront = function() {
+      return this.each(function(){
+        this.parentNode.appendChild(this);
+      });
+    };
+
+  function mouseover(d) {
+
+      d3.select(this)
+        .transition()
+        .style("stroke", "#808285")
+        .style("stroke-width", "1");
+
+      d3.select(this).moveToFront();
+
+      var data = idLookup.get(d.id);
+
+      myTooltip2
+        .style("display", null) // this removes the display none setting from it
+        .html("<p>" + data[current] + " diagnoses per 100,000 people in " + data.county + ", " + data.state +"</p>");
+
+    }
+
+    function mousemove(d) {
+      myTooltip2
+        .style("top", (d3.event.pageY - 10) + "px" )
+        .style("left", (d3.event.pageX + 10) + "px");
+      }
+
+
+    function mouseout(d) {
+      d3.select(this)
+        .transition()
+        .style("stroke", null)
+        .style("stroke-width", null);
+
+      myTooltip2.style("display", "none");  // this sets it to invisible!
+    }
+
+})();
---
+++ js/graphs/scatter5.js
@@ -0,0 +1,427 @@
+/*--------------------------------------------------------------------------
+Scatter: median income & stamp_children_rate
+--------------------------------------------------------------------------*/
+
+(function() {
+
+  /*--------------------------------------------------------------------------
+  Setup vars
+  --------------------------------------------------------------------------*/
+
+  var margin = {
+    top: 10,
+    right: 10,
+    bottom: 50,
+    left: 60
+  },
+  width = 500 - margin.right - margin.left,
+  height = 500 - margin.top - margin.bottom;
+
+
+  var data;
+  var svg;
+  var dateFormat = d3.time.format("%Y");
+  var year = [];
+  var nest = [];
+  var nest2013 = [];
+
+  var dotRadius = d3.scale.sqrt()
+  .domain([0, 22])
+  .range([0, 22]);
+
+  var dotOpacity = .7;
+  var startYear = 2005,
+  filterValue = 2005;
+
+  var myTooltip2 = d3.select("body")
+   .append("div")
+   .attr("class", "myTooltip2");
+
+  /*--------------------------------------------------------------------------
+  Scale, Axis Variables & Setup
+  --------------------------------------------------------------------------*/
+
+  var xMax = 580;
+  var yMax = 70;
+
+  var xScale = d3.scale.linear()
+  .range([margin.left, width-margin.right]);
+  var yScale = d3.scale.linear()
+  .range([margin.top, height - margin.bottom]);
+
+  var xAxis = d3.svg.axis()
+  .scale(xScale)
+  .orient("bottom")
+  .ticks(5);
+
+  var yAxis = d3.svg.axis()
+  .scale(yScale)
+  .ticks(5)
+  .tickFormat(d3.format("s"))
+  .orient("left");
+
+
+  xScale.domain([0, xMax]);
+  yScale.domain([yMax, 0]);
+
+  svg = d3.select("#vis5").append("svg")
+  .attr("viewBox", "0 0 " + width + " " + height )
+  .attr("preserveAspectRatio", "xMinYMin slice");
+
+  /*dropdown*/
+  var dropDown = d3.select("#filter5").append("select")
+                  .attr("name", "county-list");
+
+
+
+  var sliderOkay = false;
+
+
+  /*--------------------------------------------------------------------------
+  Slider
+  --------------------------------------------------------------------------*/
+  // see examples in http://www.macwright.org/chroniton/example/
+
+
+  function drawSlider() {
+    d3.select("#slider5").append('div')
+    .call(slider);
+    sliderOkay = true;
+  }
+
+  function end() {
+    console.log(slider.isAtEnd());
+    if (slider.isAtEnd()) {
+      slider.pause();
+    }
+  }
+
+  var slider;
+
+
+  d3.csv("data/dataSet.csv", function(error, data) {
+
+    console.log("datos", data);
+
+
+    /*dropdown*/
+    var options = dropDown.selectAll("option")
+             .data([{county:"All"}].concat(data))
+             .enter()
+             .append("option");
+
+
+    options.text(function (d) { return d.county; })
+    .attr("value", function (d) { return d.county; });
+
+    dropDown.on("change", function() {
+      var selected = this.value;
+      displayOthers = this.checked ? "inline" : "none";
+      display = this.checked ? "none" : "inline";
+
+      if(selected == 'All'){
+        svg.selectAll(".dots")
+            .attr("display", display);
+      }
+      else{
+        svg.selectAll(".dots")
+            .filter(function(d) {return selected != d.county;})
+            .attr("display", display)
+            .attr("fill", function (d) {
+              if (d.selection == "Top") {
+                return "rgb(184,92,87)";
+              }
+              else {
+                return "#BFBFBF";
+              }
+            })
+            .attr("opacity", function (d) {
+              if ((d.stamp_children_rate) && (d.under18bk_perc)) {
+                return dotOpacity;
+              } else {
+                return 0;
+              }
+            });
+
+        svg.selectAll(".dots")
+            .filter(function(d) {return selected == d.county;})
+            .attr("display", display)
+            .attr("opacity", 1)
+            .attr("stroke-width", 1.5)
+            .attr("stroke-opacity", 0.7)
+            .attr("stroke", "black")
+            .attr("opacity", 1)
+            .style("fill", "#D9B26E");
+      }
+  });
+
+    d3.select(window).on('resize', resize);
+
+    function resize() {
+
+
+    }
+    var swidth = parseInt(d3.select('#slider5').style('width'),10);
+    slider = chroniton()
+    .domain([dateFormat.parse("2005"), dateFormat.parse("2013")])
+    .labelFormat(d3.time.format('%Y'))
+    .width(swidth)
+    .height(52)
+    .playButton(true)
+    .playbackRate(.5) // can also be set to loop
+    .on("change", function (d) {
+      filterValue = dateFormat(d3.time.year(d));
+      console.log("filterValue", filterValue);
+      if (filterValue <= 2013) {
+        redraw(filterValue);
+        end();
+      }
+
+    });
+
+    year = ["2005", "2007", "2008", "2009", "2010", "2011", "2012", "2013"];
+
+    console.log("year", year)
+    //Loop once for each row in data
+    nest = d3.nest()
+    .key(function (d) {
+      return d.year;
+    })
+    .map(data, d3.map);
+
+    nest2013 = d3.nest()
+    .key(function (d) {
+      return d.county;
+    })
+    .map(data, d3.map);
+
+    console.log("NEST", nest);
+    console.log("nest2013", nest2013);
+
+
+    /*---------------------------------------------------------------------
+    drawAxes()
+    ---------------------------------------------------------------------*/
+    function drawAxes() {
+      yAxis.scale(yScale);
+      xAxis.scale(xScale);
+
+      svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + (height - margin.bottom) + ")")
+      .call(xAxis)
+      .append("text")
+      .attr("x", width-margin.right)
+      .attr("y", 30)
+      .attr("dy", "1em")
+      .style("text-anchor", "end")
+      .attr("class", "label")
+      .text("Children receiving food stamps (per every 1,000 children)");
+
+      svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + (margin.left) + ",0)")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -margin.top)
+      .attr("dx", "0.1em")
+      .attr("y", -45)
+      .attr("dy", "0.2em")
+      .style("text-anchor", "end")
+      .attr("class", "label")
+      .text("African American under 18");
+    }
+
+
+
+    /*--------------------------------------------------------------------------
+    drawScatter()
+    --------------------------------------------------------------------------*/
+    function drawScatter(year) {
+      year = year;
+      console.log(year);
+      var yearData = nest.get(year);
+      console.log(yearData);
+
+      /*---------------------------------------------------------------------
+      Circles
+      ---------------------------------------------------------------------*/
+      var circles = svg.selectAll("circle")
+      .data(yearData)
+      .enter()
+      .append("circle")
+      .attr("class", "dots");
+
+      circles.attr("cx", function (d) {
+        if (!isNaN(d.stamp_children_rate)) {
+          return xScale(+d.stamp_children_rate);
+        }
+      })
+      .attr("cy", function (d) {
+        if (!isNaN(d.under18bk_perc)) {
+          return yScale(+d.under18bk_perc);
+        }
+      })
+      .attr("r", function (d) {
+        if (!isNaN(d.perc_homeless)) {
+          return dotRadius(d.perc_homeless);
+        }
+      }) // you might want to increase your dotRadius
+      .attr("fill", function (d) {
+        if (d.selection == "Top") {
+          return "rgb(184,92,87)";
+        }
+        else {
+          return "#BFBFBF";
+        }
+      })
+      .attr("opacity", function (d) {
+        if ((d.stamp_children_rate) && (d.under18bk_perc)) {
+          return dotOpacity;
+        } else {
+          return 0;
+        }
+      });
+
+      circles.on("mouseover", mouseoverFunc)
+      .on("mouseout", mouseoutFunc)
+      .on("mousemove", mousemoveFunc)
+      .style('cursor','pointer');
+
+
+
+    }
+
+
+    /*--------------------------------------------------------------------------
+    Call the functions
+    --------------------------------------------------------------------------*/
+
+    drawAxes();
+    drawScatter(startYear);
+    /*setColorDomain();*/
+    drawSlider();
+
+  });
+
+  /*--------------------------------------------------------------------------
+  redraw()
+  --------------------------------------------------------------------------*/
+
+  function redraw(year) {
+
+    var year = year;
+    console.log(nest.get(year));
+    var circles = svg.selectAll("circle.dots")
+    .data(nest.get(year));
+    console.log(svg);
+
+    console.log(circles);
+
+    circles.attr("fill", function (d) {
+      if (!(d.under18bk_perc) || !(d.stamp_children_rate)) {
+        return "rgba(0, 0, 0, 0)";
+      } else {
+        if (d.selection == "Top") {
+          return "rgb(184,92,87)";
+        }
+        else {
+          return "#BFBFBF";
+        }
+      }
+    });
+
+    circles.exit()
+    .transition()
+    .duration(100)
+    .ease("exp")
+    .attr("r", 0)
+    .remove();
+    // transition -- move to proper widths and location
+    circles.transition()
+    .duration(100)
+    .ease("quad")
+    .attr("cx", function (d) {
+      if (!isNaN(d.stamp_children_rate)) {
+        return xScale(+d.stamp_children_rate);
+      }
+    })
+    .attr("cy", function (d) {
+      if (!isNaN(d.under18bk_perc)) {
+        return yScale(+d.under18bk_perc);
+      }
+    })
+    .attr("r", function (d) {
+      if (!isNaN(d.perc_homeless)) {
+        return dotRadius(d.perc_homeless);
+      }
+    }) // you might want to increase your dotRadius
+    .attr("fill", function (d) {
+      if (d.selection == "Top") {
+        return "rgb(184,92,87)";
+      }
+      else {
+        return "#BFBFBF";
+      }
+    })
+    .attr("opacity", function (d) {
+      if ((d.under18bk_perc) && (d.stamp_children_rate)) {
+        return dotOpacity;
+      } else {
+        return 0;
+      }
+    });
+
+  } // end of draw function
+
+  var legend = svg.append("g")
+  .attr("class", "legend")
+  .attr("transform", "translate(" + (width - margin.right - '18') + "," + (height - 385) + ")")
+  .selectAll("g")
+  .data([5, 15, 25])
+  .enter().append("g");
+
+  legend.append("circle")
+  .attr("cy", function(d) { return -dotRadius(d); })
+  .attr("r", dotRadius);
+
+  legend.append("text")
+  .attr("y", function(d) { return -2 * dotRadius(d); })
+  .attr("dy", "1em")
+  .text(d3.format(".1s"));
+
+  legend.append("text")
+  .attr("class", "sphere")
+  .attr("dy", "-46em")
+  .attr("dx", "-4em")
+  .attr("y", width)
+  .attr("x", height / 100)
+  .text("Homeless students (%)");
+
+  function mouseoverFunc(d) {
+    myTooltip2
+    .style("display", null) // this removes the display none setting from it
+    .html("<p>" + "<span>" + d.county + "</span>" +
+    "<br> Homeless students: " + "<em>" + d.perc_homeless + "%</em>" +
+    "<br>African American under 18: " + "<em>" + d.under18bk_perc + "%</em>" +
+    "<br>Children receiving food stamps (per every 1,000 children): " + "<em>" + d3.format("")(d.stamp_children_rate) + "</em>" + "</p>");
+
+
+    d3.selection.prototype.moveToFront = function() {
+  return this.each(function() {
+  this.parentNode.appendChild(this);
+  });
+};
+  }
+  function mousemoveFunc(d) {
+    //console.log("events", window.event, d3.event);
+    myTooltip2
+    .style("top", (d3.event.pageY - 10) + "px" )
+    .style("left", (d3.event.pageX + 15) + "px");
+  }
+  function mouseoutFunc(d) {
+    return myTooltip2.style("display", "none");  // this sets it to invisible!
+  }
+
+})();
